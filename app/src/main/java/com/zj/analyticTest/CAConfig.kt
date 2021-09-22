ea800b34c3f3e7b197e944f32b77249c84cdd200
belin zhao <zjj0888@gmail.com>
2021-09-22 10:56:33 +0800

    merge code

--- app/src/main/java/com/zj/analyticTest/CAConfig.kt
+++ app/src/main/java/com/zj/analyticTest/CAConfig.kt
@@ -1,6 +1,7 @@
 package com.zj.analyticTest
 
 import com.zj.analyticSdk.CAConfigs
+import org.json.JSONObject
 
 
 object CAConfig : CAConfigs {
@@ -14,4 +15,14 @@ object CAConfig : CAConfigs {
     override fun getServerUrl(): String {
         return "http://192.168.50.212:8081/track_new?access_token=6228feb1-2a09-432a-a923-bc6aaf89f91e"
     }
+
+
+    override fun getEventParams(eventName: String, withData: Any?, baseProperties: JSONObject): JSONObject {
+        return super.getEventParams(eventName, withData, baseProperties)
+    }
+
+    override fun beforeEvent(properties: JSONObject): JSONObject? {
+        return super.beforeEvent(properties)
+    }
+
 }
